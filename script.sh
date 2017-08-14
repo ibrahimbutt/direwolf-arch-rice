d815188f90af8532842995d815a60ebad327bd76
Ibrahim <ibrahimbutt1993@gmail.com>
2017-08-14 23:03:20 +0100

    Redo Script

--- script.sh
+++ script.sh
@@ -1,45 +1,24 @@
-#!/bin/sh
+netctl enable SKY
+reboot
 
-# ON FIRST BOOT
-
-# 1. Place this script in /usr/local/bin folder.
-# 2. Run `wifi-menu` in the terminal to make a wifi profile.
-# 3. Run `netctl enable <profile name>; reboot`.
-
-# AFTER REBOOT
-
-echo "Checking for system updates..."
 sudo pacman -Syu
-
-echo "Installing dependencies..."
-sudo pacman -S --noconfirm base-devel cairo libxcb python3 xcb-proto xcb-util-image xcb-util-wm xcb-util-xrm alsa-lib jsoncpp libmpdclient libcurl wireless_tools imagemagick feh nitrogen bgs hsetroot habak
-
-echo "Checking for system updates..."
-sudo pacman -Syu
-
+mkdir -p /tmp/pacaur_install
+cd /tmp/pacaur_install
+sudo pacman -S binutils make gcc fakeroot pkg-config --noconfirm --needed
 sudo pacman -S expac yajl git --noconfirm --needed
-
-if [ ! -n "$(pacman -Qs cower)" ]; then
-    curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
-    makepkg PKGBUILD --skippgpcheck --install --needed
-fi
-
-# Install "pacaur" from AUR
-if [ ! -n "$(pacman -Qs pacaur)" ]; then
-    curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
-    makepkg PKGBUILD --install --needed
-fi
-
-# Clean up...
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
+makepkg PKGBUILD --skippgpcheck --install --needed
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+makepkg PKGBUILD --install --needed
 cd ~
 rm -r /tmp/pacaur_install
 
-pacaur -S polybar-git
-git clone --branch 3.0.5 --recursive https://github.com/jaagr/polybar
-mkdir polybar/build
-cd polybar/build
-cmake ..
-sudo make install
-install -Dm644 /usr/share/doc/polybar/config $HOME/.config/polybar/config
+sudo pacman -Syu
+sudo pacman -S cairo libxcb python3 xcb-proto xcb-util-image xcb-util-wm xcb-util-xrm python-pip imagemagick
 
 pacaur -S python-pywal
+pacaur -S polybar-git
+
+cd ~
+cd mkdir polybar/build
+install -Dm644 /usr/share/doc/polybar/config $HOME/.config/polybar/config
